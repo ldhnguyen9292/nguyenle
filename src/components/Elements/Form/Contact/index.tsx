9f849f2f5d286b00a30c2e32c4cbf88a742113ff
Nguyen Le <ldhnguyen9292@gmail.com>
2024-11-14 15:26:54 +0700

    Feat: add ToastProvider component and integrate into layout, update ContactForm to use emailjs for message sending with notifications

--- src/components/Elements/Form/Contact/index.tsx
+++ src/components/Elements/Form/Contact/index.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { Box } from '@mui/material';
 import { useFormik } from 'formik';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
+import { globalEnv } from '@/config/globalEnv';
 import Button from '../../Button';
 import TextField from '../../TextField';
 
 const ContactForm: React.FC = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -14,35 +20,69 @@ const ContactForm: React.FC = () => {
       question: '',
     },
     validationSchema: TextField.validationSchema,
-    onSubmit: (e) => {
-      console.log('Form submitted', e);
+    onSubmit: async (value, { resetForm }) => {
+      const data = {
+        from_name: value.name,
+        to_name: 'Nguyen Le',
+        message: `${value.question}\n\nEmail: ${value.email}`,
+      };
+
+      try {
+        setIsSubmitting(true);
+        emailjs.init({ publicKey: globalEnv.EMAILJS_PUBLIC_KEY });
+        await emailjs.send(globalEnv.EMAILJS_SERVICE_ID, globalEnv.EMAILJS_TEMPLATE_TO_OWNER, data);
+        resetForm();
+        setIsSubmitting(false);
+        toast('Message sent successfully');
+      } catch (e) {
+        const error = e as EmailJSResponseStatus;
+        toast(`Failed to send message: ${error.text}`);
+      }
     },
   });
 
   return (
-    <Box
-      id="contact"
-      component="form"
-      noValidate
-      autoComplete="off"
-      className="flex flex-col items-center justify-between h-full"
-    >
-      <Box className="flex justify-between items-start w-full gap-2">
-        <TextField label="Name" name="name" formik={formik} placeholder="Enter your name" />
-        <TextField label="Email" name="email" formik={formik} placeholder="your@email.com" />
+    <>
+      <Box
+        id="contact"
+        component="form"
+        noValidate
+        autoComplete="off"
+        className="relative flex flex-col items-center justify-between h-full"
+      >
+        <Box className="flex justify-between items-start w-full gap-2">
+          <TextField label="Name" name="name" formik={formik} placeholder="Enter your name" />
+          <TextField label="Email" name="email" formik={formik} placeholder="your@email.com" />
+        </Box>
+        <TextField
+          label="Question"
+          name="question"
+          formik={formik}
+          multiline
+          rows={4}
+          placeholder="Enter question or feedback"
+        />
+        <Button
+          type="submit"
+          className="bg-tertiary-0 text-primary rounded-full m-5 border-none"
+          onClick={(e) => {
+            e.preventDefault();
+            formik.handleSubmit();
+          }}
+          disabled={isSubmitting}
+        >
+          {isSubmitting && (
+            <svg
+              className="animate-spin h-5 w-5 border-b-2 border-primary rounded-full"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            />
+          )}
+          {!isSubmitting && 'Send'}
+        </Button>
       </Box>
-      <TextField
-        label="Question"
-        name="question"
-        formik={formik}
-        multiline
-        rows={4}
-        placeholder="Enter question or feedback"
-      />
-      <Button type="submit" className="bg-tertiary-0 text-primary rounded-full m-5 border-none">
-        Send
-      </Button>
-    </Box>
+    </>
   );
 };
 
